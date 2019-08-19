d8c659ad35cd711d46377bcd25d93c4804b8bcbd
unknown <spozaf.kontakt@gmail.com>
2019-08-19 13:37:25 +0200

    apartament carousel

--- src/app/apartament-page/apartament-page.component.ts
+++ src/app/apartament-page/apartament-page.component.ts
@@ -15,18 +15,23 @@ export class ApartamentPageComponent implements OnInit {
   slides = [
     {img: "http://placehold.it/350x150/000000"},
     {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/111111"},
     {img: "http://placehold.it/350x150/333333"},
     {img: "http://placehold.it/350x150/666666"}
   ];
   slideConfig = {
   //"centerPadding": '60px',
   "centerMode": true,
-  "slidesToShow": 3, 
+  "slidesToShow": 5, 
   "slidesToScroll": 1,
   "infinite": true,
   "autoplay": true,
   "autoplaySpeed": 2000,
-  "dots": true,
+  //"dots": true,
   };
 
 }
