d741bc6f7f70e72a3d5a8b49d6ebc70721d30c68
unknown <spozaf.kontakt@gmail.com>
2019-08-19 13:36:50 +0200

    change in names

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { Component, OnInit, HostListener, Inject, Input } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { WINDOW } from './../window.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,26 +9,50 @@ import { WINDOW } from './../window.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  // scrolledClass = 'navbar__scroll-not';
-  scrolled = false;
+  isHomePage: boolean = false;
+  navbarTransparent = false;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
-    @Inject(WINDOW) private window
-  ) { }
+    @Inject(WINDOW) private window,
+    private router: Router
+  ) {
+      this.router.events.subscribe((event) => {
+        if(event['url'] && event['url'] == '/') {
+          this.isHomePage = true;
+          this.navbarTransparent = true;
+        } else if (event['url'] && event['url'] != '/') {
+          this.isHomePage = false;
+          this.navbarTransparent = false;
+        }
+      });
+   }
 
   ngOnInit() {
+    
   }
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
 
     const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (number > 100) {
-      this.scrolled = true;
+    if (number < 100 && this.isHomePage) {
+      this.navbarTransparent = true;
     } else {
-      this.scrolled = false;
+      this.navbarTransparent = false;
     }
 
+  }
+
+  onPageClick(hompage: boolean) {
+    console.log('HomePageFun ' + this.isHomePage);
+    if(hompage){
+      this.isHomePage = true;
+      this.navbarTransparent = true;
+    }
+    else{
+      this.isHomePage = false;
+      this.navbarTransparent = false;
+    }
   }
 }
