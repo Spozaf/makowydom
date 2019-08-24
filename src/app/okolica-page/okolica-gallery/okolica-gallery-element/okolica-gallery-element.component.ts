ade605f8482057d8af80cfa707a08eca3ea5eea8
unknown <spozaf.kontakt@gmail.com>
2019-08-25 00:29:50 +0200

    duża częśc okolic skończona

--- src/app/okolica-page/okolica-gallery/okolica-gallery-element/okolica-gallery-element.component.ts
+++ src/app/okolica-page/okolica-gallery/okolica-gallery-element/okolica-gallery-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-okolica-gallery-element',
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./okolica-gallery-element.component.scss']
 })
 export class OkolicaGalleryElementComponent implements OnInit {
+  @Input() photoOne: string = './../../../../assets/images/Okolica/Miasta/wieliczka1.jpg';
+  @Input() photoOnePosition: string = 'center bottom';
+  @Input() photoTwo: string = './../../../../assets/images/Okolica/Miasta/krakow1.jpg';
+  @Input() photoTwoPosition: string = 'center center';
+  @Input() photoThree: string = './../../../../assets/images/Okolica/Miasta/krakow2.jpg';
+  @Input() photoThreePosition: string = 'center center';
+  @Input() photoFour: string = './../../../../assets/images/Okolica/Miasta/nowysacz1.jpg';
+  @Input() photoFourPosition: string = 'center center';
+  @Input() photoFive: string = './../../../../assets/images/Okolica/Miasta/wieliczka2.jpg';
+  @Input() photoFivePosition: string = 'center center';
+  @Input() isLeft: boolean = true;
+  @Input() nazwa: string;
+  @Input() elementOne: string;
+  @Input() elementTwo: string;
+  @Input() elementThree: string;
 
   constructor() { }
 
