ab46f74672f2a10b09810fd744edf5f278455fe7
unknown <spozaf.kontakt@gmail.com>
2019-09-04 22:31:57 +0200

    main page skonczony

--- src/app/shared-components/button/button.component.ts
+++ src/app/shared-components/button/button.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
 
   @Input() buttonText: string;
-  @Input() isRed: boolean = true;
+  @Input() isDark: boolean = true;
 
   constructor() { }
 
