fe99d3aae948a981bc3d2407eeffd3629d65502c
unknown <spozaf.kontakt@gmail.com>
2019-09-08 09:38:39 +0200

    jeszcze pozdaki z dla-gosci

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ApartamentPageComponent } from './apartament-page/apartament-page.component';
 import { OkolicaPageComponent } from './okolica-page/okolica-page.component';
-import { DlaGosciPageComponent } from './dla-gosci-page/dla-gosci-page.component';
 import { AtrakcjePageComponent } from './atrakcje-page/atrakcje-page.component';
 import { CennikPageComponent } from './cennik-page/cennik-page.component';
 
@@ -11,7 +10,6 @@ const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'apartament', component: ApartamentPageComponent },
   { path: 'okolica', component: OkolicaPageComponent },
-  { path: 'dla-gosci', component: DlaGosciPageComponent },
   { path: 'atrakcje', component: AtrakcjePageComponent },
   { path: 'cennik', component: CennikPageComponent }
 ];
